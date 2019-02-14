f7b50ca57888eabf75072e9c3007cffcd6d54bbe
elizabethlarkinnelson <elizabeth.larkin.nelson@gmail.com>
2019-02-14 15:54:36 -0800

    DB Models IP. Image addition to fixture TBD

--- findit/jobs/models.py
+++ findit/jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    job_title = models.CharField(max_length=50)
+    company = models.IntegerField()
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=50)
+    company_logo = models.ImageField()
