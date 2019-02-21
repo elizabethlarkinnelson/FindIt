c6786399cb9a977b8be15cfab597c36b6a8903d5
elizabethlarkinnelson <elizabeth.larkin.nelson@gmail.com>
2019-02-21 15:44:08 -0800

    Views IP

--- findit/jobs/urls.py
+++ findit/jobs/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /jobs/
     path('', views.all_jobs, name='all_jobs'),
+    # ex: /jobs/1/
+    path('<int:job_id>/', views.job_detail, name='job_detail'),
 ]
--- findit/jobs/views.py
+++ findit/jobs/views.py
@@ -7,3 +7,6 @@ from .models import Job
 def all_jobs(request):
     all_jobs = Job.objects.all()
     return HttpResponse(all_jobs)
+
+def job_detail(request, job_id):
+    return HttpResponse("You're looking at job %s" % job_id)
