cff346657e41057535f4ed0d88971f2307aa9bbe
elizabethlarkinnelson <elizabeth.larkin.nelson@gmail.com>
2019-02-22 15:17:04 -0800

    Jobs and all info rendering. Need to make it pretty and add extra jobs

--- findit/jobs/views.py
+++ findit/jobs/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
 
-from .models import Job
+from .models import Job, Company
 
 
 def all_jobs(request):
     all_jobs = Job.objects.all()
+    all_companies = Company.objects.all()
     context = {
         'all_jobs': all_jobs,
     }
