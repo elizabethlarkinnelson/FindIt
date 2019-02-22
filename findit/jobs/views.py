066d7c7c843b0539ec33882e31640aba184988ce
elizabethlarkinnelson <elizabeth.larkin.nelson@gmail.com>
2019-02-22 10:14:06 -0800

    Basic views IP

--- findit/jobs/views.py
+++ findit/jobs/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 
 from .models import Job
 
 
 def all_jobs(request):
     all_jobs = Job.objects.all()
-    return HttpResponse(all_jobs)
+    context = {
+        'all_jobs': all_jobs,
+    }
+    return render(request, 'jobs/index.html', context)
 
 def job_detail(request, job_id):
-    return HttpResponse("You're looking at job %s" % job_id)
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/job_detail.html', {'job_title': job.title})
