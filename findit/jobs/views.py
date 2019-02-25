2504b45e26b64d6adcb1dcf7c3b0e509553a055f
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2019-02-24 18:01:06 -0800

    logos rendering

--- findit/jobs/views.py
+++ findit/jobs/views.py
@@ -6,12 +6,18 @@ from .models import Job, Company
 
 def all_jobs(request):
     all_jobs = Job.objects.all()
-    all_companies = Company.objects.all()
     context = {
         'all_jobs': all_jobs,
     }
     return render(request, 'jobs/index.html', context)
 
+
 def job_detail(request, job_id):
     job = get_object_or_404(Job, pk=job_id)
-    return render(request, 'jobs/job_detail.html', {'job_title': job.title})
+    company = job.company.name
+    context = {
+        'job_title': job.title,
+        'company_name': company,
+        'logo_path': 'jobs/'+ company + '.jpg'
+    }
+    return render(request, 'jobs/job_detail.html', context)
